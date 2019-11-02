86b28a288c73e0f30b8bb0e0296e36f2ff9d65cc
Mevlud <imevlud@gmail.com>
2019-11-02 20:12:28 +0300

    added some functionality

--- src/App.js
+++ src/App.js
@@ -1,61 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
 import PlayListCard from "./PlayListCard";
+
+import Scroll from "react-scroll";
 
 import logo from "./logo.svg";
 import classes from "./App.module.css";
 
-import { VideoMetaData, VideoPlayList } from "./VideoData";
+import { VideoMetaData, VideoPlayList, VideoMetaDataList } from "./VideoData";
 
-function App() {
-  const videoMetaData = VideoMetaData;
-  const videoPlayList = VideoPlayList;
+class App extends Component {
+  state = {
+    currentVideoCardPos: 0,
+    videoMetaData: VideoMetaData
+  };
 
-  const videoPlayListRender = videoPlayList.map(item => {
+  videoPlayList = VideoPlayList;
+
+  onVideoCardClick = pos => {
+    this.setState({
+      videoMetaData: VideoMetaDataList[pos],
+      currentVideoCardPos: pos
+    });
+    Scroll.animateScroll.scrollToTop();
+  };
+
+  render() {
+    const videoPlayListRender = this.videoPlayList.map((item, pos) => {
+      return (
+        <PlayListCard
+          key={item.id}
+          thumbnail={item.thumbnail}
+          title={item.title}
+          alt={item.title}
+          onCardClick={this.onVideoCardClick}
+          cardPos={pos}
+          currentVideo={this.state.currentVideoCardPos}
+        />
+      );
+    });
+
     return (
-      <PlayListCard
-        key={item.id}
-        thumbnail={item.thumbnail}
-        title={item.title}
-        alt={item.title}
-      />
-    );
-  });
-
-  return (
-    <div className={classes.App}>
-      <div className={classes.MainWrapper}>
-        <div className={classes.PlayerSection}>
-          <div className={classes.VideoBlock}>
-            <iframe
-              className={classes.VideoPlayer}
-              src="https://player.vimeo.com/video/190062231"
-              frameBorder="0"
-              webkitallowfullscreen=""
-              mozallowfullscreen=""
-              allowFullScreen=""
-            ></iframe>
-          </div>
-
-          <div className={classes.DataBlock}>
-            <div className={classes.VideoStats}>
-              <p className={classes.ViewCount}>{videoMetaData.views} views</p>
-              <div>
-                <i className="far fa-heart" aria-hidden="true"></i>
-                <i className="far fa-comment-alt" aria-hidden="true"></i>
-                <i className="far fa-bookmark" aria-hidden="true"></i>
-              </div>
+      <div className={classes.App}>
+        <div className={classes.MainWrapper}>
+          <div className={classes.PlayerSection}>
+            <div className={classes.VideoBlock}>
+              <iframe
+                className={classes.VideoPlayer}
+                src={`https://player.vimeo.com/video/${this.state.videoMetaData.vimeoId}`}
+                frameBorder="0"
+                webkitallowfullscreen=""
+                mozallowfullscreen=""
+                allowFullScreen=""
+              ></iframe>
             </div>
-            <hr />
-            <h1 className={classes.VideoTitle}>{videoMetaData.title}</h1>
-            <p className={classes.VideoDescription}>
-              {videoMetaData.description}
-            </p>
+
+            <div className={classes.DataBlock}>
+              <div className={classes.VideoStats}>
+                <p className={classes.ViewCount}>
+                  {this.state.videoMetaData.views} views
+                </p>
+                <div>
+                  <i className="far fa-heart" aria-hidden="true"></i>
+                  <i className="far fa-comment-alt" aria-hidden="true"></i>
+                  <i className="far fa-bookmark" aria-hidden="true"></i>
+                </div>
+              </div>
+              <hr />
+              <h1 className={classes.VideoTitle}>
+                {this.state.videoMetaData.title}
+              </h1>
+              <p className={classes.VideoDescription}>
+                {this.state.videoMetaData.description}
+              </p>
+            </div>
           </div>
+          <div className={classes.PlayList}>{videoPlayListRender}</div>
         </div>
-        <div className={classes.PlayList}>{videoPlayListRender}</div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/PlayListCard.js
+++ src/PlayListCard.js
@@ -2,8 +2,16 @@ import React from "react";
 import classes from "./PlayListCard.module.css";
 
 const PlayListCard = props => {
+  const classesArr = [classes.PlayListCard];
+  if (props.currentVideo === props.cardPos) {
+    classesArr.push(classes.Active);
+  }
+
   return (
-    <div className={classes.PlayListCard}>
+    <div
+      className={classesArr.join(" ")}
+      onClick={() => props.onCardClick(props.cardPos)}
+    >
       <img src={props.thumbnail} alt={props.title} />
       <h3>{props.title}</h3>
     </div>
